3778a1d3fd7bbce72b9ec637a227bd21c6628d5f
Wilder Pereira <wilder_roberto@hotmail.com>
2016-04-04 21:47:26 -0300

    Exercício 6

---
+++ TPilha.c
@@ -0,0 +1,23 @@
+/*
+6)	Implemente uma função chamada TPilha, que receba um vetor de inteiros com 15 elementos. Para cada um deles, como segue:
+- se o número for par, insira-o na pilha;
+- se o número lido for ímpar, retire um número da pilha;
+- Ao final, esvazie a pilha imprimindo os elementos.
+*/
+
+void TPilha(int array[15]){
+	Pilha stack;
+	int i;
+	for(i = 0; i<15; i++){
+		if(i%2==0){
+			push(&stack, i);
+		}else{
+			if(!isEmpty(&stack)){
+				pop(&stack);
+			}	
+		}
+	}
+	while(!isEmpty(&stack)){
+		printf("%d",&pop);
+	}
+}
---
+++ TPilha2.c
@@ -0,0 +1,6 @@
+/*
+7)	Escreva uma função chamada TPilha2 que recebe como parâmetro 2 pilhas (N e P) e um vetor de inteiros. Para cada um:
+- se positivo, inserir na pilha P;
+- se negativo, inserir na pilha N;
+- se zero, retirar um elemento de cada pilha.
+*/
