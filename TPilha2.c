4eca9225a95c61a1383d8aaa2a78b4d30e4f8254
Wilder Pereira <wilder_roberto@hotmail.com>
2016-04-04 21:51:47 -0300

    Exercício 7

--- TPilha2.c
+++ TPilha2.c
@@ -4,3 +4,17 @@
 - se negativo, inserir na pilha N;
 - se zero, retirar um elemento de cada pilha.
 */
+
+void TPilha2(Pilha N, Pilha P, int array[]){	
+	int tamanho = sizeof array / sizeof array[0];
+	for(i = 0; i<tamanho; i++){
+		if(i>0){
+			push(&P, array[i]);
+		}else if(i<0){
+			push(&N, array[i]);
+		}else{
+			pop(&P);
+			pop(&N);
+		}
+	}
+}
